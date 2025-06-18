a955c86dc3c6a1d8ef8ec19e7d285f5f16d9437f
loganrenz <narduk@mac.com>
2025-06-18 00:08:30 -0400

    ok

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -3,10 +3,9 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
       <div class="flex justify-between h-16 items-center">
         <router-link to="/"
-          class="flex items-center space-x-3 group focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg">
+          class="flex items-center space-x-3 group focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-lg transition-transform duration-150 ease-in-out hover:translate-x-1 hover:scale-105 active:scale-100">
           <img src="/icons/icon.svg" alt="Logo" class="h-9 w-9" />
-          <span class="text-2xl font-extrabold tracking-tight text-blue-900 group-hover:underline">Stock Pick
-            Game</span>
+          <span class="text-2xl font-extrabold tracking-tight text-blue-900">Stock Pick Game</span>
         </router-link>
         <div class="flex items-center space-x-6">
           <router-link to="/stats" class="text-blue-700 font-bold hover:underline text-lg">Stats</router-link>
